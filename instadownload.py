ebd5319941b719b4774788610f8133bcf1c25bcf
Praveen Vino <31967668+praveenvino39@users.noreply.github.com>
2020-06-01 21:12:08 +0530

    Update instadownload.py

--- instadownload.py
+++ instadownload.py
@@ -14,7 +14,7 @@ def requests_start_url(start_url):
  
 def find_photo_url(requests_url):
     try:
-        soup = BeautifulSoup(requests_url, 'html.parser')
+        soup = BeautifulSoup(requests_url)
         photo_url = soup.find("meta", property="og:image")
         return photo_url["content"]
     except Exception as e:
