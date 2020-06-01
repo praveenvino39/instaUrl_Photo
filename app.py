38c4bd73684e1dff41d7241dec8545771bec10b6
Praveen Vino <31967668+praveenvino39@users.noreply.github.com>
2020-06-01 19:21:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import instadownload as ig
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def homepage():
+    return 'It works'
+
+
 @app.route('/<path:url>')
 def index(url):
     photo_url = {'photo_url': ig.main(url)}
     return jsonify(photo_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
